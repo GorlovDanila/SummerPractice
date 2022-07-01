d09b680ba0401900b936df700d3ced236872467f
GorlovDanila <danildanil20030@gmail.com>
2022-07-01 13:43:34 +0300

    .

--- app/src/main/java/com/example/summer_practice/MainActivity.kt
+++ app/src/main/java/com/example/summer_practice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.summer_practice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun calculation() {
         val name: TextView? = findViewById(R.id.personName)
         val weight: TextView? = findViewById(R.id.personWeight)
@@ -25,12 +27,15 @@ class MainActivity : AppCompatActivity() {
         val result: TextView = findViewById(R.id.result)
 
         try {
-            if(name == null || weight == null || height == null || age == null || name.text.length > 50 || height.text.toString().toInt() !in 1..249 || weight.text.toString().toDouble() !in 1.0..249.0 || age.text.toString().toInt() !in 1..149){
+            if((name == null) || (weight == null) || (height == null) || (age == null) || (name.text.length > 50) || (height.text.toString()
+                    .toInt() !in (1..249)) || (weight.text.toString()
+                    .toDouble() !in (1.0..249.0)) || (age.text.toString().toInt() !in (1..149))
+            ){
                 result.text = "Данные введены некорректно"
             } else {
                 val res: Double =
-                    88.36 + (13.4 * weight?.text.toString().toDouble()) + (4.8 * height?.text.toString()
-                        .toInt()) - (5.7 * age?.text.toString().toInt()) + name.text.length
+                    88.36 + (13.4 * weight.text.toString().toDouble()) + (4.8 * height.text.toString()
+                        .toInt()) - (5.7 * age.text.toString().toInt()) + name.text.length
                 result.text = "Ответ: ${res.toInt()}"
             }
         } catch (e: Exception){
